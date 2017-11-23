3843c3098fbb947d5f91922fa67cbff15af06953
Yoshiaki Sato <virtualgimbal@xa2.so-net.ne.jp>
2017-11-23 23:55:28 +0900

    Delete4

--- scripts/module.sh
+++
@@ -1,3 +0,0 @@
-file=`rospack find architect_driver`/include/iMCs01_driver/urbtc.ko
-sudo rmmod urbtc.ko
-sudo insmod ${file}
--- scripts/setWiressLan.sh
+++
@@ -1,3 +0,0 @@
-sudo ip route del default
-sudo ip route add default via 192.168.43.1 dev wlan0
-ip route show
